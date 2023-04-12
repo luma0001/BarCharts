f0ca78a289de36c1854532df01c8992d1cbd6e1b
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-04-12 11:34:20 +0200

    set column through array
    vi lavede også arrayet btw

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 
 window.addEventListener("load", start);
 
+const data = [
+  23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23,
+  18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12,
+];
+
 function start() {
-  console.log("Running");
+  //   console.log(data.length());
   const bars = document.querySelectorAll(".bar");
 
   const firstBar = bars[0];
 
-  const h = 50;
+  const h = (data[0] / 23) * 100;
 
   firstBar.style.height = `${h}px`;
 }
