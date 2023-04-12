0df4761a3333541e1c0d759c0f05b163ba6deeda
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-04-12 11:36:43 +0200

    fix

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ window.addEventListener("load", start);
 
 const data = [
   23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23,
-  18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12,
+  18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3,
+  5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12, 23, 18, 3, 5, 12,
 ];
 
 function start() {
-  //   console.log(data.length());
   const bars = document.querySelectorAll(".bar");
 
   const firstBar = bars[0];
 
-  const h = (data[0] / 23) * 100;
+  const h = (data[0] / 32) * 100;
 
   firstBar.style.height = `${h}px`;
 }
